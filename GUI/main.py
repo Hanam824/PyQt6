c1327ae49fc589075d1b9822f264aae416ba8d4f
Hanam824 <58266242+Hanam824@users.noreply.github.com>
2024-06-12 08:28:51 +0900

    revert

--- GUI/main.py
+++ GUI/main.py
@@ -1,119 +1,119 @@
-# from PySide6.QtWidgets import QApplication
-# from window import MainWindow 
+from PySide6.QtWidgets import QApplication
+from window import MainWindow 
 
-# # Only needed for access to command line arguments
-# import sys
-
-# # You need one (and only one) QApplication instance per application.
-# # Pass in sys.argv to allow command line arguments for your app.
-# # If you know you won't use command line arguments QApplication([])  works too.
-# app = QApplication(sys.argv)
-
-# # Create a Qt widget, which will be our window.
-# window = MainWindow()
-# window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-
-# # Start the event loop.
-# sys.exit(app.exec())
-
-
-# # Your application won't reach here until you exit and the event
-# # loop has stopped.
-
+# Only needed for access to command line arguments
 import sys
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
-from PySide6.QtGui import *
 
-class mainwindow(QMainWindow):
-    def __init__(self, parent = None):
-        super(mainwindow, self).__init__(parent = parent)
-        self.title = 'SUDOKU SOLVER'
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])  works too.
+app = QApplication(sys.argv)
 
-        self.initUI()
+# Create a Qt widget, which will be our window.
+window = MainWindow()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        # add menubar
-        menubar = self.menuBar()
+# Start the event loop.
+sys.exit(app.exec())
 
-        # add drop down items
-        exitAct = QAction('&Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit Application')
-        exitAct.triggered.connect(qApp.quit)
 
-        newAct = QAction('New', self)
-        newAct.setShortcut('Ctrl+N')
-        newAct.setStatusTip('New Sudoku')
-        newAct.triggered.connect(GameLogic.clearFields)
+# Your application won't reach here until you exit and the event
+# loop has stopped.
 
-        rulesAct = QAction('Rules', self)
-        rulesAct.setShortcut('Ctrl+R')
-        rulesAct.setStatusTip('Sudoku Rules')
-        rulesAct.triggered.connect(GameLogic.sudokuRules)
+# import sys
+# from PySide6.QtWidgets import *
+# from PySide6.QtCore import *
+# from PySide6.QtGui import *
 
-        # add menubar entries
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(newAct)
-        fileMenu.addAction(exitAct)
+# class mainwindow(QMainWindow):
+#     def __init__(self, parent = None):
+#         super(mainwindow, self).__init__(parent = parent)
+#         self.title = 'SUDOKU SOLVER'
 
-        helpMenu = menubar.addMenu('&Help')
-        helpMenu.addAction(rulesAct)
+#         self.initUI()
 
-        # call gridlayout function
-        l = self.gridLayout()
-        self.setCentralWidget(l)
+#     def initUI(self):
+#         self.setWindowTitle(self.title)
+#         # add menubar
+#         menubar = self.menuBar()
 
-        self.show()
+#         # add drop down items
+#         exitAct = QAction('&Exit', self)
+#         exitAct.setShortcut('Ctrl+Q')
+#         exitAct.setStatusTip('Exit Application')
+#         exitAct.triggered.connect(qApp.quit)
 
-    def gridLayout(self):
-        layout = QGridLayout()
-        solve = QPushButton('Solve', self)
-        solve.clicked.connect(GameLogic.solveSudoku)
-        solve.setFixedSize(60, 30)
+#         newAct = QAction('New', self)
+#         newAct.setShortcut('Ctrl+N')
+#         newAct.setStatusTip('New Sudoku')
+#         newAct.triggered.connect(GameLogic.clearFields)
 
-        mainwindow.fields = {}
+#         rulesAct = QAction('Rules', self)
+#         rulesAct.setShortcut('Ctrl+R')
+#         rulesAct.setStatusTip('Sudoku Rules')
+#         rulesAct.triggered.connect(GameLogic.sudokuRules)
 
-        # validate user input
-        onlyInt = QIntValidator(1, 9, self)
+#         # add menubar entries
+#         fileMenu = menubar.addMenu('&File')
+#         fileMenu.addAction(newAct)
+#         fileMenu.addAction(exitAct)
 
-        # this is the part that doesnt work...
-        for x in range(9):
-            for y in range(9):
-                # keep a reference to the buttons
-                mainwindow.fields[(x, y)] = QLineEdit(self)
-                mainwindow.fields[(x, y)].setMaxLength(1)
-                mainwindow.fields[(x, y)].setValidator(onlyInt)
-                mainwindow.fields[(x, y)].setFixedSize(60, 60)
-                mainwindow.fields[(x, y)].setFont(QFont('Sans Serif', 20))
-                mainwindow.fields[(x, y)].setAlignment(Qt.AlignCenter)
-                # add to the layout
-                layout.addWidget(mainwindow.fields[(x, y)], x, y)
+#         helpMenu = menubar.addMenu('&Help')
+#         helpMenu.addAction(rulesAct)
 
-        layout.addWidget(solve, 10, 4)
-        w = QWidget()
-        w.setLayout(layout)
-        return(w)
+#         # call gridlayout function
+#         l = self.gridLayout()
+#         self.setCentralWidget(l)
 
-class GameLogic():
+#         self.show()
 
-    def clearFields(self):
-        for i in range(9):
-            for j in range(9):
-                mainwindow.fields[(i, j)].clear()
+#     def gridLayout(self):
+#         layout = QGridLayout()
+#         solve = QPushButton('Solve', self)
+#         solve.clicked.connect(GameLogic.solveSudoku)
+#         solve.setFixedSize(60, 30)
 
-    def sudokuRules(self):
-        pass
+#         mainwindow.fields = {}
 
-    def solveSudoku(self):
-        pass
+#         # validate user input
+#         onlyInt = QIntValidator(1, 9, self)
 
-def main():
-    app = QApplication(sys.argv)
-    ex = mainwindow()
+#         # this is the part that doesnt work...
+#         for x in range(9):
+#             for y in range(9):
+#                 # keep a reference to the buttons
+#                 mainwindow.fields[(x, y)] = QLineEdit(self)
+#                 mainwindow.fields[(x, y)].setMaxLength(1)
+#                 mainwindow.fields[(x, y)].setValidator(onlyInt)
+#                 mainwindow.fields[(x, y)].setFixedSize(60, 60)
+#                 mainwindow.fields[(x, y)].setFont(QFont('Sans Serif', 20))
+#                 mainwindow.fields[(x, y)].setAlignment(Qt.AlignCenter)
+#                 # add to the layout
+#                 layout.addWidget(mainwindow.fields[(x, y)], x, y)
 
-    sys.exit(app.exec_())
+#         layout.addWidget(solve, 10, 4)
+#         w = QWidget()
+#         w.setLayout(layout)
+#         return(w)
 
-if __name__ == '__main__':
-    main()
+# class GameLogic():
+
+#     def clearFields(self):
+#         for i in range(9):
+#             for j in range(9):
+#                 mainwindow.fields[(i, j)].clear()
+
+#     def sudokuRules(self):
+#         pass
+
+#     def solveSudoku(self):
+#         pass
+
+# def main():
+#     app = QApplication(sys.argv)
+#     ex = mainwindow()
+
+#     sys.exit(app.exec_())
+
+# if __name__ == '__main__':
+#     main()
